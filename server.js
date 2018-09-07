7c55cecb97b9468ab82edf6debe50e8fd0c1463e
Rongqing ZHOU <489688547@qq.com>
2018-09-06 22:25:35 -0700

    use node&express and add nodemailer form

---
+++ server.js
@@ -0,0 +1,51 @@
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req,res) {
+    res.sendFile(process.cwd() + '/public/index.html')
+});
+app.get('/projects', function (req,res) {
+    res.sendFile(process.cwd() + '/public/projects.html')
+});
+app.get('/contact', function (req,res) {
+    res.sendFile(process.cwd() + '/public/contact.html')
+});
+
+
+app.post('/contact', function (req, res) {
+  let mailOpts, smtpTrans;
+  smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+          user: 'zhourongqing0518@gmail.com',
+          pass: 'pmrunflfhwloeynx' 
+      }
+  });
+
+  mailOpts = {
+    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+    to: 'zhourongqing0518@gmail.com',
+    subject: 'New message from contact form at rongqingzhou.com',
+    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+  };
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+    if (error) {
+      res.render('contact-failure');
+    }
+    else {
+      res.render('contact-success');
+    }
+  });
+});
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000...');
+});
